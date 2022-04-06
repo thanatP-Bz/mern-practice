9978b42e7b103cacd9a89a85d0d90e0683d7effb
Thanat <bztzn512@gmail.com>
2022-04-05 20:16:13 -0400

    Landing

---
+++ src/pages/Landing.js
@@ -0,0 +1,25 @@
+import React from "react";
+import designer from "../assets/designer.svg";
+
+const Landing = () => {
+  return (
+    <div className="flex h-screen justify-between items-center">
+      <div>
+        <img src={designer} alt="designer" />
+      </div>
+      <div className="w-full max-w-screen-sm m-7">
+        <h1 className="text-3xl mb-5">Let Explore My Awesome Memories</h1>
+        <p className="mb-7">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum veritatis
+          consequuntur dolor eius error asperiores accusantium aliquid debitis
+          cumque vitae.
+        </p>
+        <button className="bg-orange-300 px-7 py-2 rounded text-white hover:bg-slate-600">
+          Register/Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
